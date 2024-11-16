c0903b25affff9eecc647eed2e85c4697916be48
lvaldes0980 <vald682@msn.com>
2024-11-16 10:18:36 -0500

    Challenege01 completed

--- RevitAddingBootcamp_Orientation/cmdChallenge01.cs
+++ RevitAddingBootcamp_Orientation/cmdChallenge01.cs
@@ -1,4 +1,7 @@
-﻿namespace RevitAddingBootcamp_Orientation
+﻿using Autodesk.Revit.DB;
+using System.Windows.Forms;
+
+namespace RevitAddingBootcamp_Orientation
 {
     [Transaction(TransactionMode.Manual)]
     public class cmdChallenge01 : IExternalCommand
@@ -11,8 +14,106 @@
             Document doc = uidoc.Document;
 
             // Your Module 01 Challenge code goes here
-            // Delete the TaskDialog below and add your code
-            TaskDialog.Show("Module 01 Challenge", "Coming Soon!");
+            int myFirstNumber = 250;
+            double startingElevation = 0;
+            double floorHeight = 15;
+
+            startingElevation += floorHeight;
+
+            // create a transation to lock the model
+            Transaction t = new Transaction(doc);
+            t.Start("I'm solving the challenge question");
+
+            for (int i = 1; i < myFirstNumber; i++)
+
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    // get a tittle block type
+                    FilteredElementCollector collector2 = new FilteredElementCollector(doc);
+                    collector2.OfCategory(BuiltInCategory.OST_TitleBlocks);
+                    collector2.WhereElementIsElementType();
+
+                    // create a sheet
+                    ViewSheet newSheet = ViewSheet.Create(doc, collector2.FirstElementId());
+                    newSheet.Name = "FIZZBUZZ_#" + i;
+
+                    //create a floor level
+                    Level newLevel = Level.Create(doc, floorHeight);
+
+                    // create a filtered element collector to get view family type
+                    FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+                    collector1.OfClass(typeof(ViewFamilyType));
+
+                    ViewFamilyType floorPlanVFT = null;
+                    foreach (ViewFamilyType curVFT in collector1)
+                    {
+                        if (curVFT.ViewFamily == ViewFamily.FloorPlan)
+                        {
+                            floorPlanVFT = curVFT;
+                        }
+                    }
+                    // create a floor plan view
+                    ViewPlan newFloorPlan = ViewPlan.Create(doc, floorPlanVFT.Id, newLevel.Id);
+
+                    // create a viewport
+                    // first create a point
+                    XYZ placePoint = new XYZ();
+                    Viewport newViewport = Viewport.Create(doc, newSheet.Id, newFloorPlan.Id, placePoint);
+                }
+                else if (i % 3 == 0)
+                {
+                    //create a floor level
+                    Level newLevel = Level.Create(doc, floorHeight);
+
+                    // create a filtered element collector to get view family type
+                    FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+                    collector1.OfClass(typeof(ViewFamilyType));
+
+                    ViewFamilyType floorPlanVFT = null;
+                    foreach (ViewFamilyType curVFT in collector1)
+                    {
+                        if (curVFT.ViewFamily == ViewFamily.FloorPlan)
+                        {
+                            floorPlanVFT = curVFT;
+                        }
+                    }
+                    // create a floor plan view
+                    ViewPlan newFloorPlan = ViewPlan.Create(doc, floorPlanVFT.Id, newLevel.Id);
+                    newFloorPlan.Name = "FIZZ_" + i;
+
+                   
+                }
+                else if (i % 5 == 0)
+                {
+                    //create a floor level
+                    Level newLevel = Level.Create(doc, floorHeight);
+
+                    // create a filtered element collector to get view family type
+                    FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+                    collector1.OfClass(typeof(ViewFamilyType));
+
+                    ViewFamilyType ceilingPlanVFT = null;
+                    foreach (ViewFamilyType curVFT in collector1)
+                    {
+                        if (curVFT.ViewFamily == ViewFamily.CeilingPlan)
+                        {
+                            ceilingPlanVFT = curVFT;
+                        }
+                    }
+                    // create a ceiling plan view
+                    ViewPlan newCeilingPlan = ViewPlan.Create(doc, ceilingPlanVFT.Id, newLevel.Id);
+                    newCeilingPlan.Name = "BUZZ_" + i;
+                }
+
+               
+
+
+            }
+            t.Commit();
+            t.Dispose();
+
+
 
 
             return Result.Succeeded;
--- RevitAddingBootcamp_Orientation/cmdSkills01.cs
+++ RevitAddingBootcamp_Orientation/cmdSkills01.cs
@@ -10,10 +10,63 @@
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
-            // Your Module 01 Skills code goes here
-            // Delete the TaskDialog below and add your code
-            TaskDialog.Show("Module 01 Skills", "Got Here!");
+            // create a transation to lock the model
+            Transaction t = new Transaction(doc);
+            t.Start("I'm doing something new in Revit");
 
+
+            // get a tittle block type
+            FilteredElementCollector collector2 = new FilteredElementCollector(doc);
+            collector2.OfCategory(BuiltInCategory.OST_TitleBlocks);
+            collector2.WhereElementIsElementType();
+
+            // create a sheet
+            ViewSheet newSheet = ViewSheet.Create(doc, collector2.FirstElementId());
+            newSheet.Name = "My new sheet";
+            newSheet.SheetNumber = "A101";
+
+            //create a floor level
+            Level newLevel = Level.Create(doc, 10);
+
+            // create a filtered element collector to get view family type
+            FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+            collector1.OfClass(typeof(ViewFamilyType));
+
+            ViewFamilyType floorPlanVFT = null;// why was this set to null?
+            foreach (ViewFamilyType curVFT in collector1) // what is curVFT?
+            {
+                if (curVFT.ViewFamily == ViewFamily.FloorPlan)
+                {
+                    floorPlanVFT = curVFT;
+                }
+            }
+            // create a floor plan view
+            ViewPlan newFloorPlan = ViewPlan.Create(doc, floorPlanVFT.Id, newLevel.Id);
+            newFloorPlan.Name = "My new floor plan";
+
+            ViewFamilyType ceilingPlanVFT = null;
+            foreach (ViewFamilyType curVFT in collector1)
+            {
+                if(curVFT.ViewFamily == ViewFamily.CeilingPlan)
+                {
+                    ceilingPlanVFT = curVFT;
+                }
+            }
+
+            // create a ceiling plan view
+            ViewPlan newCeilingPlan = ViewPlan.Create(doc, ceilingPlanVFT.Id, newLevel.Id);
+            newCeilingPlan.Name = "My new ceiling plan";
+
+
+            // create a viewport
+            // first create a point
+            XYZ insPoint = new XYZ();
+            XYZ insPoint2 = new XYZ(1, 0.5, 0);
+
+            Viewport newViewport = Viewport.Create(doc, newSheet.Id, newFloorPlan.Id, insPoint2);
+
+            t.Commit();
+            t.Dispose();
 
             return Result.Succeeded;
         }
